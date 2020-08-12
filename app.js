5ae20d82b27ca44c62c46b3edf91602d7312f7e0
Nafisa Hasan <nafisahasan98@gmail.com>
2020-08-12 21:36:37 +0600

    added routes and design pages

--- app.js
+++ app.js
@@ -6,12 +6,27 @@ const bodyParser = require("body-parser");
 
 //initialize
 var app = express();
+
+//set up views for different route
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public"))); //using public folder for bootstrap
 
 app.get("/", function (req, res) {
-  console.log("Hello World!"); //this will run in the server
-  res.send("Hello World!"); //this will run for client/browser
+  // console.log("Hello World!"); //this will run in the server
+  // res.send("Hello World!"); //this will run for client/browser
+
+  res.render("index", { title: "Welcome" });
 });
+app.get("/about", function (req, res) {
+  res.render("about");
+});
+app.get("/contact", function (req, res) {
+  res.render("contact");
+});
+
 app.listen(3000);
 console.log("server is running on port 3000");
